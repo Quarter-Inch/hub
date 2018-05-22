3a2ad31a287ae59dd600944cf6e99f1a17bd344d
yambal <jy@yambal.net>
2018-05-22 22:53:28 +0900

    js

--- docs/js/googleLogin.js
+++ docs/js/googleLogin.js
@@ -11,7 +11,7 @@
 
 var provider = new firebase.auth.GoogleAuthProvider();
 provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
-firebase.auth().useDeviceLanguage();
+firebase.auth().languageCode = 'pt';
 firebase.auth().signInWithPopup(provider).then(function(result) {
   // This gives you a Google Access Token. You can use it to access the Google API.
   var token = result.credential.accessToken;
